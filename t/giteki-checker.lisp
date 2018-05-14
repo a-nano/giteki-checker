56eec0ce92cbccf9c1e2b0d04456000d493fb355
Akihide Nano <an74abc@gmail.com>
2018-05-14 10:36:30 +0900

    added test

--- t/giteki-checker.lisp
+++ t/giteki-checker.lisp
@@ -7,8 +7,11 @@
 
 ;; NOTE: To run this test file, execute `(asdf:test-system :giteki-checker)' in your Lisp.
 
-(plan nil)
+(setf *enable-colors* nil)
 
-;; blah blah blah.
+(plan 2)
+
+(ok (check "018-160054"))
+(is (check "000-000000") '(nil nil nil nil) :test #'equalp)
 
 (finalize)
